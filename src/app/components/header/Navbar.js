b8c6d3a07f6bb4f501dd8624183982c75aa0c6a2
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-05-23 12:31:17 +0200

    navlink highlight

--- src/app/components/header/Navbar.js
+++ src/app/components/header/Navbar.js
@@ -1,33 +1,27 @@
-"use client"
-import Link from 'next/link'
+
 import React from 'react'
+import NavLink from './NavLink'
+
+const navbarLinks = [
+   {
+      href: "/meals",
+      title: "Browse Meals"
+   },
+   {
+      href: "/community",
+      title: "Foodies Community"
+   },
+]
 
 function Navbar() {
-
-   const linkStyleHover = `hover:bg-gradient-to-r hover:from-[#ff8a05] hover:to-[#f9b331] hover:text-transparent hover:bg-clip-text`
-
-   const navbarLinks = [
-      {
-         href: "/meals",
-         title: "Browse Meals"
-      },
-      {
-         href: "/community",
-         title: "Foodies Community"
-      },
-   ]
-
 
    return (
       <nav>
          <ul className='flex gap-6 text-xl font-mono flex-wrap '>
             {navbarLinks.map(({ href, title }) => (
-               <li key={title}>
-                  <Link
-                     className={`font-bold px-2 py-4 rounded-sm text-text ${linkStyleHover} `}
-                     href={href}>{title}
-                  </Link>
-               </li>
+               <NavLink key={title} href={href} >
+                  {title}
+               </NavLink>
             ))}
          </ul>
       </nav>
